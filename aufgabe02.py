cac0aa76d4ecf98023a7184f6fcbd7948e94a26e
Aidandos <timon.willi@gmail.com>
2017-04-25 21:19:02 +0200

    First Draft of Submission
    First draft of submission

---
+++ aufgabe02.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PCL II, Uebung 4, FS17
+# Aufgabe 1
+
+import bz2
+from lxml import etree
+import itertools
+import argparse
+from tempfile import mkstemp
+from shutil import move
+from os import remove, close
+import random
+import fileinput
+import os
+
+
+def gettitles(infile, testfile, trainfile, k, exitCondition):
+    """infile: Korpus der schon durch bz2 geöffnet wurde
+        testfile: Ausgabedatei für k zufällig ausgewählte Artikeltitel
+        trainfile: Ausgabedatei für alle anderen Titel
+        We apply Reservoir Sampling that has been presented in the lecture.
+        The counter starts counting after a sufficient amount of test data has
+        been collected."""
+
+    if os.path.exists(testfile):
+        os.remove(testfile)
+    if os.path.exists(trainfile):
+        os.remove(trainfile)
+
+    exit_bool = False
+    try:
+        exit_bool = type(int(exitCondition)) is int
+    except TypeError:
+        # print('Either the break condition is missing or exitCondition must be a number')
+        pass
+
+    counter_exit = 0
+    i = 0
+    tree = etree.iterparse(
+        bz2.open(infile), tag='{http://www.mediawiki.org/xml/export-0.10/}title')
+
+    test = open(testfile, 'a')
+    for event, elem in itertools.islice(tree, k):
+        test.write(elem.text + "\n")
+        i += 1
+        # print('%s\n' % elem.text.encode('utf-8'))
+        elem.clear()
+        for element in elem.xpath('ancestor-or-self::*'):
+            while element.getprevious() is not None:
+                del element.getparent()[0]
+
+    test.close()
+
+    train = open(trainfile, 'a', encoding='utf-8')
+    try:
+        for event, elem in itertools.islice(tree, k + 1, None):
+            r = random.randint(0, i)
+            if r < k:
+                replaceLine(testfile, elem.text, r)
+            else:
+                train.write(elem.text + "\n")
+            i += 1
+            elem.clear()
+            for element in elem.xpath('ancestor-or-self::*'):
+                while element.getprevious() is not None:
+                    del element.getparent()[0]
+            if exit_bool:
+                counter_exit += 1
+                if counter_exit > int(exitCondition):
+                    raise ExitCondition
+    except ExitCondition:
+        pass
+
+    train.close()
+
+
+def replaceLine(temp_file, replacement, placeOfLine):
+    """Adaption from http://stackoverflow.com/questions/39086/search-and-replace-a-line-in-a-file-in-python
+    and http://stackoverflow.com/questions/16622754/how-do-you-replace-a-line-of-text-in-a-text-file-python"""
+
+    fh, abs_path = mkstemp()
+    with open(abs_path, 'w', encoding="utf-8") as output_file, open(temp_file) as input_file:
+        n = 0
+        for line in input_file:
+            if n == placeOfLine:
+                output_file.write(replacement + "\n")
+            else:
+                output_file.write(line + "\n")
+            n += 1
+
+    close(fh)
+    # Remove original file
+    remove(temp_file)
+    # Move new file
+    move(abs_path, temp_file)
+
+
+class ExitCondition(Exception):
+    """Helps to show results earlier"""
+    pass
+
+
+def main():
+    # Get the input and output filenames as well as sample size from
+    # commandline
+    parser = argparse.ArgumentParser(
+        description='Choose k random titles from dump')
+    parser.add_argument('--infile', '-i', dest='infile')
+
+    parser.add_argument('--testfile', '-te', dest="testfile")
+
+    parser.add_argument('--trainfile', '-tr', dest="trainfile")
+
+    parser.add_argument('--samplesize', '-k', dest="k")
+
+    parser.add_argument('--exitcondition', '-e',
+                        dest="exitCondition",
+                        required=False)
+
+    args = parser.parse_args()
+
+    gettitles(args.infile, args.testfile, args.trainfile,
+              int(args.k), args.exitCondition)
+
+
+if __name__ == '__main__':
+    main()
---
+++ aufgabe1.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PCL II, Uebung 4, FS17
+# Aufgabe 1
+
+import glob
+from lxml import etree
+import argparse
+import operator
+import os
+
+
+def getfreqwords(indir, outfile, exitCondition):
+    """gets 20 most frequent sentences"""
+    # checks if exit condition is present
+    exit_bool = False
+    try:
+        exit_bool = type(int(exitCondition)) is int
+    except TypeError:
+        #print('Either the break condition is missing or exitCondition must be a number')
+        pass
+
+    # definition of variables, tmp_file is for memory stabilization
+    tmp_file = 'tmp_file.txt'
+    counter_exit = 0
+    counter_top_20 = 0
+    sentenceKeys = {}
+
+    with open(tmp_file, 'wb') as tmp_file_io:
+
+        tmp_file_io.write(b'<?xml version="1.0" encoding="UTF-8"?><root>')
+        # xpath is not as pretty as regex :/
+        pattern = indir + '/SAC-Jahrbuch_[0-9][0-9][0-9][0-9]_mul.xml'
+
+        try:
+            # iterating through sentences to get lemmas
+            for file in glob.glob(pattern):
+                tree = etree.parse(file)
+                # structure of xml
+                sentences = tree.xpath('/book/article/div/s')
+                for sentence in sentences:
+                    if len(sentence.xpath('w/@lemma')) >= 6:
+                        sentenceConcat = ' '.join(sentence.xpath('w/@lemma'))
+                        key = hash(sentenceConcat)
+                        if key not in sentenceKeys:
+                            sentenceKeys[key] = 1
+                            # creating element tree so it can be parsed again
+                            # later
+                            el = etree.Element('sentence', hash=str(
+                                key), value=sentenceConcat)
+                            tmp_file_io.write(etree.tostring(el))
+                        else:
+                            sentenceKeys[key] += 1
+                    if exit_bool:
+                        counter_exit += 1
+                        if counter_exit > int(exitCondition):
+                            raise ExitCondition
+
+        except ExitCondition:
+            pass
+        tmp_file_io.write(b'</root>')
+
+    # re-using temp file for parsing, now sentence has hash
+    sentences_with_hash = etree.parse(tmp_file)
+    with open(outfile, 'w', encoding="utf-8") as output:
+
+        # sorting sentences such that most frequent is on top
+        for key in sorted(sentenceKeys.items(), key=operator.itemgetter(1), reverse=True):
+            counter_top_20 += 1
+            if counter_top_20 > 20:
+                break
+            else:
+                output.writelines(sentences_with_hash.xpath(
+                    '(/root/sentence[@hash=%d]/@value)[1]' % key[0])[0] + '\n')
+
+
+if os.path.exists(tmp_file):
+    os.remove(tmp_file)
+
+
+class ExitCondition(Exception):
+    """Helps to show results earlier"""
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Find most frequent sentences.')
+    parser.add_argument('--indir', '-i',
+                        dest='indir',)
+
+    parser.add_argument('--outfile', '-o',
+                        dest="outfile")
+
+    parser.add_argument('--exitcondition', '-e',
+                        dest="exitCondition",
+                        required=False)
+
+    args = parser.parse_args()
+    getfreqwords(args.indir, args.outfile, args.exitCondition)
+
+
+if __name__ == '__main__':
+    main()
